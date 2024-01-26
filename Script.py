41408fc3d3ae5cee32768a363c7aba4db1723fc3
llazzarotto <llazzarotto@smag.tech>
2024-01-26 13:33:18 +0100

    feat: pitie que ca s'arrete

--- Script.py
+++ Script.py
@@ -1,7 +1,9 @@
 import string
+import random
 
 def compter_lettres(fichier):
-    compteur_lettres = dict.fromkeys(string.ascii_lowercase, 0)
+    alphabet = string.ascii_lowercase + "#" + "@"
+    compteur_lettres = dict.fromkeys(alphabet, 0)
     total_lettres = 0
 
     with open(fichier, 'r', encoding='utf-8') as f:
@@ -19,7 +21,7 @@ def compter_lettres(fichier):
     return probabilites
 
 def compter_lettres_precedentes(fichier):
-    alphabet = string.ascii_lowercase + "#" + "@"
+    alphabet = string.ascii_lowercase
     compteur_lettres = {lettre: {precedente: 0 for precedente in alphabet} for lettre in alphabet}
     total_lettres = 0
 
@@ -63,16 +65,59 @@ def compter_double_lettres_precedentes(fichier):
                                 compteur_lettres[lettre_globale][paire_precedentes] += 1
                                 total_lettres[lettre_globale] += 1
 
-            print(lettre_globale, " : ", total_lettres[lettre_globale])
-
     probabilites = {lettre: {paire_precedentes: (compteur / total_lettres[lettre])*100 if total_lettres[lettre] != 0 else 0 for paire_precedentes, compteur in compteur_lettres[lettre].items()} for lettre in alphabet}
 
     return probabilites
 
+def calcul_propa_lettre_suivante(double_lettres, fichier):
+    probabilites_double_lettres_precedentes = compter_double_lettres_precedentes(fichier)
+    lettres_freq = {}
+    total_freq = 0
+
+    for lettre, double_lettre_stat in probabilites_double_lettres_precedentes.items():
+        for doubles_lettres_key, double_lettres_freq_key in double_lettre_stat.items():
+            if doubles_lettres_key == double_lettres:
+                lettres_freq[lettre] = double_lettres_freq_key
+                total_freq += double_lettres_freq_key
+    
+    if total_freq == 0:
+        return {lettre: 0 for lettre in lettres_freq}
+    else:
+        proba = {lettre: (lettre_freq / total_freq)*100 for lettre, lettre_freq in lettres_freq.items()}
+        return proba
+    
+
+    
+
+def generer_mot_aleatoire(fichier):
+    probabilites_lettres = compter_lettres(fichier)
+
+    alphabet = list(probabilites_lettres.keys())
+    mot = "#" + random.choices(alphabet, weights=probabilites_lettres.values(), k=1)[0]
+    mot_fini = False
+    i = 2
+    
+    while not mot_fini:
+        double_lettres_precedentes = mot[i-2] + mot[i-1]
+
+        lettre_suivante_proba = calcul_propa_lettre_suivante(double_lettres_precedentes, fichier)
+        print(double_lettres_precedentes," : ",lettre_suivante_proba)
+        lettre_suivante = random.choices(alphabet, weights=lettre_suivante_proba.values(), k=1)[0]
+        if lettre_suivante != "@":
+            mot += lettre_suivante
+            i += 1
+        else:
+            mot_fini = True
+
+    print(mot)
+    return mot
 
 
 
-# probabilites_lettres = compter_lettres('./liste_francais.txt')
+
+
+
+probabilites_lettres = compter_lettres('./liste_francais.txt')
 
 # for lettre, probabilité in probabilites_lettres.items():
 #     print(f"La lettre '{lettre}' apparaît avec une probabilité de {probabilité:.4f}")
@@ -89,8 +134,10 @@ def compter_double_lettres_precedentes(fichier):
 
 probabilites_double_lettres_precedentes = compter_double_lettres_precedentes('./liste_francais.txt')
 
-for lettre, double_precedentes in probabilites_double_lettres_precedentes.items():
-    print(f"Lettre: {lettre}")
-    for double_precedentes, proba in double_precedentes.items():
-        print(f"  Précédente: {double_precedentes}, Fréquence: {proba:.8f}%")
-    print("----")
+# for lettre, double_precedentes in probabilites_double_lettres_precedentes.items():
+#     print(f"Lettre: {lettre}")
+#     for double_precedentes, proba in double_precedentes.items():
+#         print(f"  Précédente: {double_precedentes}, Fréquence: {proba:.8f}%")
+#     print("----")
+
+# print(generer_mot_aleatoire('./liste_francais.txt'))
